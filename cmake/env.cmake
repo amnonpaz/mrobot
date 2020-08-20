4e68d4cbae7ee60ab33fad84cfaecec9460152cf
Amnon Paz <pazamnon@gmail.com>
2020-08-21 00:50:11 +0300

    CMake: Use g++ with c++17 standard and extended errors

--- cmake/env.cmake
+++ cmake/env.cmake
@@ -1,3 +1,6 @@
+set(CMAKE_CXX_COMPILER "g++")
+set(CMAKE_CXX_STANDARD 17)
+add_compile_options(-Wall -Wextra -pedantic -Werror)
+
 include(${CMAKE_CURRENT_LIST_DIR}/prepend.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/lint.cmake)
-
