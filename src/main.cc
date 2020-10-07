9133514ecdedbcfb88236458e4b6038c675b1ccb
Amnon Paz <pazamnon@gmail.com>
2020-10-08 01:44:13 +0300

    Start Robot in main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,25 @@
-#include <iostream>
+#include "robot.hpp"
 
-int main()
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+
+#define MAX_CONF_FILENAME_LEN 256UL
+
+int main(int argc, const char *argv[])
 {
     std::cout << "Mayyan's Robot v" << VERSION << std::endl;
+
+    if (argc < 2) {
+        std::cout << "Please provide configuration file name" << '\n';
+        return 1;
+    }
+
+    const std::string confFilename{argv[1], std::min(::strlen(argv[1]), MAX_CONF_FILENAME_LEN)};
+
+    mrobot::Robot robot(confFilename);
+
+    robot.init();
 
     return 0;
 }
