71c1ad6d720da8ac201b2e40e86b644057f8ae4d
Amnon Paz <pazamnon@gmail.com>
2021-03-22 23:52:25 +0200

    comm: Remove receiver

--- src/comm.hpp
+++ src/comm.hpp
@@ -8,14 +8,6 @@ namespace mrobot {
 
 namespace comm {
 
-class Receiver {
-    public:
-        Receiver() = default;
-        virtual ~Receiver() = default;
-
-        virtual void receive(std::shared_ptr<unsigned char> payload, ::size_t size) = 0;
-};
-
 class Client {
     public:
         Client() = default;
@@ -26,8 +18,6 @@ class Client {
 
         virtual bool connect() = 0;
         virtual bool disconnect() = 0;
-
-        virtual void addReceiver(Receiver *receiver) = 0;
 
         virtual bool send(std::shared_ptr<std::vector<unsigned char>> payload, ::size_t size) = 0;
 };
