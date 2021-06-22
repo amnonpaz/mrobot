65160cf0e3929fca7ccf175fb8c44ef6a6403690
Amnon Paz <pazamnon@gmail.com>
2021-06-23 01:00:12 +0300

    CMake: Add option for turning off linter

--- cmake/env.cmake
+++ cmake/env.cmake
@@ -3,5 +3,7 @@ set(CMAKE_CXX_STANDARD 17)
 add_compile_options(-Wall -Wextra -pedantic -Werror)
 
 include(${CMAKE_CURRENT_LIST_DIR}/prepend.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/lint.cmake)
+if (LINT)
+    include(${CMAKE_CURRENT_LIST_DIR}/lint.cmake)
+endif ()
 include(${CMAKE_CURRENT_LIST_DIR}/tests.cmake)
